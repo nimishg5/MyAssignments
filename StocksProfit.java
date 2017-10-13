f6c9ddd19b6222d20123cad08b36d23787be7799
nimishg5 <nimishgupta1992@yahoo.in>
2017-10-13 20:44:31 +0530

    Update StocksProfit.java

--- StocksProfit.java
+++ StocksProfit.java
@@ -1,22 +1,3 @@
-/*public class StocksProfit{
-	public static void main(String args[]){
-		//int profitForDays[] = {100,200,50};
-		int profitForDays[] = {100,-100};
-		int maxProfit = 0, minSellingPrice = profitForDays[0];
-		for(int i=1;i<profitForDays.length;i++){
-			if(minSellingPrice<profitForDays[i]){
-				if(maxProfit<profitForDays[i] - minSellingPrice){
-					maxProfit = profitForDays[i] - minSellingPrice;
-				}
-			}else{
-				minSellingPrice = profitForDays[i];
-			}
-		}
-		System.out.println("maxProfit:"+maxProfit);
-		
-	}
-} */
-
 public class StocksProfit {
 	public static void main(String[] args){
 	//	int arr[] = {100,20,30,40,110,500};
